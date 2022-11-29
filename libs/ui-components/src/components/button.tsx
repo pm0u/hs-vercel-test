362f54382be2b53437b9c3f6b0c17a91c740991c
Paul Mourer <pmourer@joinhandshake.com>
2022-11-28 17:21:46 -0700

    Simplify button example type definition

--- libs/ui-components/src/components/button.tsx
+++ libs/ui-components/src/components/button.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from "react"
-import { cva, VariantProps } from "class-variance-authority"
+import { cva } from "class-variance-authority"
 
-interface ButtonBaseProps {
+export interface ButtonProps {
   /** Content displayed in the button */
   children: ReactNode
   /** Controls button appearance (color, background, font weight) */
@@ -22,10 +22,6 @@ const button = cva(['py-4 px-8 rounded-md font-bold shadow-sm'], {
     }
   }
 })
-
-export interface ButtonProps
-  extends Omit<VariantProps<typeof button>, 'variant'>,
-    ButtonBaseProps {}
 
 export const Button = ({ children, variant = "primary" }: ButtonProps) => {
   return <button className={button({ variant })}>{children}</button>

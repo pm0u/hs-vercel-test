8129c230c4659eb5e8b962950a39dcb1f485880b
Jeff Ballinger <jeffballinger@gmail.com>
2022-12-13 15:41:33 -0500

    eta 2021 winner card

---
+++ libs/ui-components/src/components/eta-2021/winners-card.stories.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { ETA2021WinnersCard } from "./winners-card"
+
+export default {
+  title: "Components/Winners Card",
+  component: ETA2021WinnersCard,
+} as ComponentMeta<typeof ETA2021WinnersCard>
+
+const Template: ComponentStory<typeof ETA2021WinnersCard> = (args) => (
+  <div className="mx-auto w-max">
+    <ETA2021WinnersCard {...args} />
+  </div>
+)
+
+export const Card = Template.bind({})
+
+Card.args = {
+  color: "coral",
+  title: 'Accounting',
+  link: 'See the winners!',
+  icon: 'https://i0.wp.com/joinhandshake.com/wp-content/uploads/2021/03/eta-industry-accounting-1.png?resize=150%2C150&ssl=1',
+}
---
+++ libs/ui-components/src/components/eta-2021/winners-card.tsx
@@ -0,0 +1,51 @@
+import React, { ReactNode } from "react"
+import { cva } from "class-variance-authority"
+import Link from "next/link"
+import { ETA2021Button } from "./button";
+
+export interface WinnersCardProps {
+  /** Background color for the card*/
+  color: "coral" | "periwinkle" | "lightBlue" | "lightGreen" | "yellow";
+  title: string,
+  /** URL to the image file */
+  icon: string,
+  /** Label text for button */
+  link: string,
+  href: string
+}
+
+const card = cva(["p-8"],
+  {
+    variants: {
+      color: {
+        coral: ["bg-legacy-coral"],
+        periwinkle: ["bg-legacy-periwinkle"],
+        lightBlue: ["bg-legacy-light-blue"],
+        lightGreen: ["bg-legacy-green-300"],
+        yellow: ["bg-legacy-yellow"],
+      }
+    },
+  }
+)
+
+export const ETA2021WinnersCard = ({
+  color = "coral",
+  title = "",
+  link = "",
+  href = "",
+  icon = ""
+}: WinnersCardProps) => {
+  return (
+    <div className={card({ color })}>
+      <h3 className="mb-10">{title}</h3>
+      <div className="flex justify-between">
+        <div className="pr-5">
+          <ETA2021Button children={link} color="white" size="small" href={href}></ETA2021Button>
+        </div>
+        <div>
+          <img src={icon} className="h-10"/>
+        </div>
+      </div>
+    </div>
+  )
+}

7f4f7ea4408f3024597a25cd25c93e25d680593f
Jeff Ballinger <jeffballinger@gmail.com>
2022-12-14 16:04:42 -0500

    formatting

--- libs/ui-components/src/components/eta-2022/index.ts
+++ libs/ui-components/src/components/eta-2022/index.ts
@@ -1,3 +1,3 @@
-import { ETA2022PastWinnerBadge } from "./past-winner-badge";
+import { ETA2022PastWinnerBadge } from "./past-winner-badge"
 
 export { ETA2022PastWinnerBadge }
--- libs/ui-components/src/components/eta-2022/past-winner-badge.tsx
+++ libs/ui-components/src/components/eta-2022/past-winner-badge.tsx
@@ -2,9 +2,10 @@ import React from "react"
 
 export const ETA2022PastWinnerBadge = () => {
   return (
-      <div className="bg-eta2022-fuschia p-2">
-        <span className="text-neutral-0 font-bold whitespace-nowrap text-sm uppercase">Past Winner</span>
-      </div>
-    
+    <div className="bg-eta2022-fuschia p-2">
+      <span className="text-neutral-0 font-bold whitespace-nowrap text-sm uppercase">
+        Past Winner
+      </span>
+    </div>
   )
 }

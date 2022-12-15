134126ecbd7a7da193854e68e12d022755f744f1
Jeff Ballinger <jeffballinger@gmail.com>
2022-12-15 10:34:11 -0500

    more lint

--- libs/ui-components/src/components/eta-2022/social-tile.tsx
+++ libs/ui-components/src/components/eta-2022/social-tile.tsx
@@ -11,7 +11,8 @@ export const ETA2022SocialTile = ({
 }: ETA2022SocialTileProps) => {
   return (
     <div className="bg-legacy-purple rounded-md h-12 w-12 p-3 text-neutral-0 flex items-center transform duration-30 hover:scale-105 hover:opacity-90 cursor-pointer">
-      {/*FIXME: using <SocialIcon> as a child of <Link> results with the following error:
+      {/*
+        FIXME: using <SocialIcon> as a child of <Link> results with the following error:
         Warning: Function components cannot be given refs.
         Attempts to access this ref will fail. 
         Did you mean to use React.forwardRef()?

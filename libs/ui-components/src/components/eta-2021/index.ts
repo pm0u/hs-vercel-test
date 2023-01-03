5bca27b78b690c764c7f688fa3862d32ff9d4c0c
Jeff Ballinger <jeffballinger@gmail.com>
2023-01-03 15:36:03 -0500

    formatting

--- libs/ui-components/src/components/eta-2021/index.ts
+++ libs/ui-components/src/components/eta-2021/index.ts
@@ -2,4 +2,4 @@ import { Breadcrumbs } from "./breadcrumbs"
 import { ETA2021Button } from "./button"
 import { ETA2021WinnersCard } from "./winners-card"
 
-export { Breadcrumbs, ETA2021Button, ETA2021WinnersCard}
+export { Breadcrumbs, ETA2021Button, ETA2021WinnersCard }
